a6867f65642d44642d82bcb403aa024854226753
einalem4 <melanie@codehangar.io>
2021-01-09 00:40:27 -0700

    password length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,25 @@
 // Assignment code here
+//how many characters would you like your password to conatin
+//click ok to confirm including special characters
+//click ok to confirm including numeric characters
+//click ok to confirm including lowercase characters
+//click ok to confirm including uppercase characters
+
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var special = ['!', '@', '#', '$', '%', '=', '&', '*', '?', '_', '"', '(', '(', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '[', ']', '^', '{', '|', '}', '~', '/',];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var generatePassword = function () {
+
+  var passLength = window.prompt("how many characters would you like your password to conatin?");
+
+  while (passLength <= 7 || passLength >= 129) {
+    window.alert("Password length must be between 8-128 characters. Try again");
+    break;
+  }
+}; 
+
 
 
 // Get references to the #generate element
