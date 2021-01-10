fb8446a60ce10dc34ed44ec01f4d0404ed49e881
einalem4 <melanie@codehangar.io>
2021-01-10 00:00:51 -0700

    randomized password is generated

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,10 @@
 // Assignment code here
-//how many characters would you like your password to conatin
-//click ok to confirm including special characters
-//click ok to confirm including numeric characters
-//click ok to confirm including lowercase characters
-//click ok to confirm including uppercase characters
-
-//length of password is between 8-129 characters
-//confirm user wants upper case
-//confirm user wants lower case
-//confirm user wants numbers
-//confirm user wants special characters
-//based on selections user is generated a password
-
 
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var special = ['!', '@', '#', '$', '%', '=', '&', '*', '?', '_', '"', '(', '(', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '[', ']', '^', '{', '|', '}', '~', '/',];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var passLength = "";
+
 
 var randomNumber = function (min, max) {
   var value = Math.floor(Math.random() * (max - min) + min);
@@ -26,26 +13,31 @@ var randomNumber = function (min, max) {
 };
 
 var generatePassword = function () {
-  var passLength = window.prompt("How many characters would you like your password to conatin?");
+  //ask how long they want password to be
+  var passLength = window.prompt("How many characters would you like your password to contain?");
 
+  //make sure the password is between 8 -128 characters
   if (passLength <= 7 || passLength >= 129) {
     window.alert("Password length must be between 8-128 characters. Try again");
     return;
   }
 
+  //ask additional criteria questions
   var upperCaseAlert = window.confirm("click OK to confirm including uppercase characters.");
   var lowerCaseAlert = window.confirm("click OK to confirm including lowercase characters.");
   var numberAlert = window.confirm("click OK to confirm including numeric characters.");
   var specialAlert = window.confirm("click OK to confirm including special characters.");
 
+  //if they answer no to all options, prompt to pick at least one
   while (upperCaseAlert === false && lowerCaseAlert === false && numberAlert === false && specialAlert === false) {
     alert("You must choose at least one option");
-    break;
+    var upperCaseAlert = window.confirm("click OK to confirm including uppercase characters.");
+    var lowerCaseAlert = window.confirm("click OK to confirm including lowercase characters.");
+    var numberAlert = window.confirm("click OK to confirm including numeric characters.");
+    var specialAlert = window.confirm("click OK to confirm including special characters.");
   }
 
-  for (var i = 0; i < passLength.length; i++); {
-    console.log(passLength);
-  }
+  var password = ""
 
   var characters = []
 
@@ -61,9 +53,14 @@ var generatePassword = function () {
   if (specialAlert === true) {
     characters = characters.concat(special)
   }
-  console.log(characters);
-};
 
+  //length of the password based on response
+  for (var i = 0; i < passLength; i++) {
+    var random = randomNumber(0, characters.length);
+    password += characters[random];
+  }
+  return password;
+};
 
 
 // Get references to the #generate element
